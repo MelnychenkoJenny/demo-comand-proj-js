d37d49f8ef21891be116c72f0face11d2d101982
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-04-13 00:00:34 +0300

    start file

---
+++ src/index.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Filmoteka</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.1.0/modern-normalize.min.css"
+      integrity="sha512-wpPYUAdjBVSE4KJnH1VR1HeZfpl1ub8YT/NKx4PuQ5NmX2tKuGu6U/JRp5y+Y8XG2tV+wKQpNHVUX03MfMFn9Q=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="./main.css" />
+  </head>
+  <body>
+    <footer class="footer">
+        <div class="container footer-container">
+        <span class="footer__text">&#169  2023 | All Rights Reserved | </span>
+        <div class="footer__box">
+            <span>Developed with </span>
+            <svg class="footer__icon" width="14" height="13">
+                <use href="./icons/symbol-defs.svg#icon-heart"></use>
+            </svg>
+            <span>by 
+                <a href="#" class="footer__link"> GoIT Students</a>
+            </span>
+        </div>
+    </div>
+        </footer>
+
+  </body>
+</html>
